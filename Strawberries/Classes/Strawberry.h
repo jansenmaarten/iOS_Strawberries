fa19085e694c9f26b60652eb053c938d9380426c
Maarten Jansen <jansen.maarten@me.com>
2012-12-13 14:50:23 +0100

    Added Strawberry class

---
+++ Strawberries/Classes/Strawberry.h
@@ -0,0 +1,16 @@
+//
+//  Strawberry.h
+//  Strawberries
+//
+//  Created by Maarten Jansen on 13/12/12.
+//  Copyright (c) 2012 iCapps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Strawberry : NSObject
+
+// Properties
+@property (nonatomic, strong) NSString *taste;
+
+@end
---
+++ Strawberries/Classes/Strawberry.m
@@ -0,0 +1,13 @@
+//
+//  Strawberry.m
+//  Strawberries
+//
+//  Created by Maarten Jansen on 13/12/12.
+//  Copyright (c) 2012 iCapps. All rights reserved.
+//
+
+#import "Strawberry.h"
+
+@implementation Strawberry
+
+@end
